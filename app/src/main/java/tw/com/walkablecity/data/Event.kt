5e4dc84eb17414b1c94299c247fa292c0948eeba
James <der.zehlenteufel@gmail.com>
2020-07-01 19:48:42 +0800

    Add data classes and init map fragment in home fragment

---
+++ app/src/main/java/tw/com/walkablecity/data/Event.kt
@@ -0,0 +1,4 @@
+package tw.com.walkablecity.data
+
+class Event {
+}
---
+++ app/src/main/java/tw/com/walkablecity/data/RouteRating.kt
@@ -0,0 +1,10 @@
+package tw.com.walkablecity
+
+data class RouteRating (
+    val coverage: Float,
+    val rest: Float,
+    val scenery: Float,
+    val snack: Float,
+    val tranquility: Float,
+    val vibe: Float
+)
