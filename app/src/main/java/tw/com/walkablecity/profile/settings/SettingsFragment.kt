b6200db1ebb1c21c0c8a7612fff8ea50c4202896
James <der.zehlenteufel@gmail.com>
2020-07-17 11:05:09 +0800

    add switch toasts in settings fragment

--- app/src/main/java/tw/com/walkablecity/profile/settings/SettingsFragment.kt
+++ app/src/main/java/tw/com/walkablecity/profile/settings/SettingsFragment.kt
@@ -8,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 
 import tw.com.walkablecity.R
 import tw.com.walkablecity.UserManager
+import tw.com.walkablecity.Util.makeShortToast
 import tw.com.walkablecity.databinding.FragmentSettingsBinding
 import tw.com.walkablecity.ext.getVMFactory
 
@@ -27,11 +29,31 @@ class SettingsFragment : Fragment() {
             .inflate(inflater, R.layout.fragment_settings, container, false)
         binding.lifecycleOwner = this
 
+        binding.afterMealSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            viewModel.afterMealSwitch(isChecked)
+        }
 
+        binding.goodWeatherSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            viewModel.goodWeatherSwitchOn(isChecked)
+        }
 
         binding.viewModel = viewModel
 
         binding.user = UserManager.user
+
+        viewModel.notifyAfterMeal.observe(viewLifecycleOwner, Observer {
+            it?.let{isChecked->
+                if(isChecked) makeShortToast(R.string.after_meal_on)
+                else makeShortToast(R.string.after_meal_off)
+            }
+        })
+
+        viewModel.notifyGoodWeather.observe(viewLifecycleOwner, Observer {
+            it?.let{isChecked->
+                if(isChecked) makeShortToast(R.string.good_weather_on)
+                else makeShortToast(R.string.good_weather_off)
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/tw/com/walkablecity/profile/settings/SettingsViewModel.kt
+++ app/src/main/java/tw/com/walkablecity/profile/settings/SettingsViewModel.kt
@@ -1,8 +1,27 @@
 package tw.com.walkablecity.profile.settings
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import tw.com.walkablecity.data.source.WalkableRepository
 
 class SettingsViewModel(val walkableRepository: WalkableRepository) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _notifyAfterMeal = MutableLiveData<Boolean>(false)
+    val notifyAfterMeal: LiveData<Boolean> get() = _notifyAfterMeal
+
+    private val _notifyGoodWeather = MutableLiveData<Boolean>(false)
+    val notifyGoodWeather: LiveData<Boolean> get() = _notifyGoodWeather
+
+
+    fun afterMealSwitch(isChecked: Boolean){
+        _notifyAfterMeal.value = isChecked
+    }
+
+    fun goodWeatherSwitchOn(isChecked: Boolean){
+        _notifyGoodWeather.value = isChecked
+    }
+
+
+
 }
